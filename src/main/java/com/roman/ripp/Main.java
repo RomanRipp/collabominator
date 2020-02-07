32ce64dcad8c1bb4783cab3d5ec8d728029feed1
RomanRipp <romanripp@RomanMac.local>
2020-02-06 20:42:05 -0800

    add a check for google credentials setting

--- src/main/java/com/roman/ripp/Main.java
+++ src/main/java/com/roman/ripp/Main.java
@@ -4,12 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("You have to provide path to credentials file.");
+            System.out.println("You have to provide path to codecollaborator credentials file.");
             return;
         }
 
-        var credentialsPath = args[0];
-        var reviewObserver = new ReviewObserver(credentialsPath);
+        var googleAppCredentialsEnvVar = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        if (googleAppCredentialsEnvVar == null)
+        {
+            System.out.println("You need to set GOOGLE_APPLICATION_CREDENTIALS environment variable." +
+                    " It needs to point to the credentials file. Example: " +
+                    "export GOOGLE_APPLICATION_CREDENTIALS=/Users/AdolfHitler/collabominator/res/gsecret.json");
+            return;
+        }
+
+        var ccbCredentialsPath = args[0];
+        var reviewObserver = new ReviewObserver(ccbCredentialsPath);
         reviewObserver.RunOnce();
 
         System.exit(0);
